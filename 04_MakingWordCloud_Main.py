f8f2ef5ceaaedfe562a2e1e3f50f05a497097cce
문영식 <162241674+YoungsikMoon@users.noreply.github.com>
2024-04-09 20:55:58 +0900

    Update 04_MakingWordCloud_Main.py

--- 04_MakingWordCloud_Main.py
+++ 04_MakingWordCloud_Main.py
@@ -3,13 +3,14 @@
 
 import pandas as pd
 import numpy as np
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
 
 data = pd.read_csv('/content/filtered_review.csv') #워드클라우드 파일 불러오기
 
 # 다시 numpy 배열로 변환합니다.
 filtered_review = np.array(filtered_review)
 
-from wordcloud import WordCloud
 # 워드 클라우드 생성 및 출력
 fontpath = '/usr/share/fonts/truetype/nanum/NanumBarunGothic.ttf'
 wordcloud = WordCloud(font_path=fontpath, background_color='white', width=800, height=800, stopwords=set(stopwords)).generate(' '.join(filtered_review))
